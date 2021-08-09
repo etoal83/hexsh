1690af284ee3bba773104a93e1d83c53ab14974f
etoal83 <etoal83@gmail.com>
2021-08-09 16:00:16 +0900

    Add single shell process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Error: Failed to read line");
+
+    let command = input.trim();
+
+    Command::new(command)
+        .spawn()
+        .expect("Error: Failed to spawn process");
 }
